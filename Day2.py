653c3c602fc0e5f072c1a6d97a8060222d399db6
AbisolaMajeed <abisola.majeed@gmail.com>
2024-08-22 00:52:00 +0100

    initial commit

---
+++ Day2.py
@@ -0,0 +1,14 @@
+# Variables
+FavouriteNumber = 2000000
+print(f"My favourite number is : {FavouriteNumber}")
+
+# List example
+fruits = ["apple", "banana", "cherry"]
+print(f"My favourite fruit is {fruits[1]}")
+
+# Dictionary example
+contacts = {
+    "Alice": "555-1234",
+    "Bob": "555-5678"
+}
+print(f"Alice's phone number is {contacts['Alice']}")
